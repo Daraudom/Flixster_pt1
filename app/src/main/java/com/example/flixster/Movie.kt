e8d8729b145053af2d03e86a588302e97e46f9f7
Daraudom <daraudomnhem@gmail.com>
2022-09-26 23:35:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.flixster
+import com.google.gson.annotations.SerializedName
+
+class Movie {
+    @SerializedName("title")
+    var title : String? = null
+
+    @SerializedName("overview")
+    var overview : String? = null
+
+    @SerializedName("poster_path")
+    var imageUrl : String? = null
+
+}
